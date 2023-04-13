34c2e0fb1b130239ceb6667ba6a125573a6fa19c
RahulRajput143 <Rahul.12107183@lpu.in>
2023-04-14 00:22:05 +0530

    first commit

---
+++ Untitled-2.py
@@ -0,0 +1,24 @@
+# %%
+print("My name is rahul")
+
+# %%
+rahul="HelloVai"
+
+
+# %%
+
+import pandas as pd
+
+data1 = {"Name": ["Alice", "Bob", "Charlie"],
+         "Age": [20, 18, 19]}
+df1 = pd.DataFrame(data1)
+
+data2 = {"Name": ["David", "Eve", "Frank"],
+         "Age": [21, 22, 20]}
+df2 = pd.DataFrame(data2)
+
+appended_df = pd.concat([df1, df2], ignore_index=True)
+
+print(appended_df)
+print("Garvit")
+
